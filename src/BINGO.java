26aad24eb4bfa4a8622f93873ccf5f42a8478061
quint <kieryquintjohn@gmail.com>
2024-02-15 22:51:32 +0800

    Introduced clearing of terminal.

--- src/BINGO.java
+++ src/BINGO.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.Random;
 
@@ -10,35 +11,38 @@ public class BINGO {
     static String rolledNumbersRepr = "";
     static String bingoCardRepr = "";
     static String bingoWinningPattern;
-    
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
         // WELCOME SCREEN
+        System.out.println("Tara BINGO!");
+        System.out.println("Press ENTER to play.");
+        scanner.nextLine();
 
         // HELP MODULE
-        System.out.println("nothing");
+//        playTutorial();
+
         // INITIALIZATION OF VARIABLES
         createBingoCard();
 
         // MAIN GAME LOOP
         letsPlayBingo();
 
-        // 5*5 grid, center is blank
         String numbers = "";
         String alreadyRolledNumbers = "";
     }
 
-    static void letsPlayBingo() {
+    static void letsPlayBingo() throws IOException, InterruptedException {
+        char letter;
         char randomNumberRepr;
         int randomNumber;
 
         while (true) {
-            char letter;
             randomNumberRepr = getRandomNumberRepr(0, ASCIIMAX);
             randomNumber = randomNumberRepr - 33;
+
             if (rolledNumbersRepr.contains(randomNumberRepr+"")) {
                 continue;
-            }
-            if (randomNumber > 1 && randomNumber <= 15) {
+            } else if (randomNumber > 1 && randomNumber <= 15) {
                 letter = 'B';
             } else if (randomNumber > 15 && randomNumber <= 30) {
                 letter = 'I';
@@ -49,39 +53,42 @@ public class BINGO {
             } else {
                 letter = 'O';
             }
-            System.out.println("Taya taya... Sa letra sang " + letter + "... " + randomNumber + "!");
-            if (bingoCardRepr.contains(randomNumberRepr+"")) {
-                System.out.println("May " + randomNumber + "!");
-            } else {
-                System.out.println("Wala " + randomNumber + ".");
 
+            System.out.println("Taya taya... (Press ENTER to roll) ");
+            scanner.nextLine();
+            System.out.println("Sa letra sang " + letter + "... " + randomNumber + "!");
+            if (bingoCardRepr.contains(randomNumberRepr+"")) {
+                System.out.println("May " + randomNumber + "!\n");
+            } else {
+                System.out.println("Wala " + randomNumber + ".\n");
             }
-            rolledNumbersRepr += (randomNumberRepr) + " ";
+
+            rolledNumbersRepr += randomNumberRepr + " ";
             printBingoCard();
             System.out.println("\nPress enter to roll again. ");
             scanner.nextLine();
+            cls();
         }
     }
 
     static void createBingoCard() {
-        int max, min;
         char randNumberRepr;
-        String sep = "\t\t\t\t\t";
+        String sep = "\t\t";
 
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; ) {
-                if (i == 2 && j == 2) {
-                    bingoCardRepr += " " + sep;
-                    j++;
-                    continue;
+        for (int row = 0; row < 5; row++) {
+            int col = 0;
+            while (col < 5) {
+                if (row == 2 && col == 2) {
+                    bingoCardRepr += ' ' + sep;
+                } else {
+                    randNumberRepr = getRandomNumberRepr((col + 1) * 15, col * 15 + 2);
+                    if (bingoCardRepr.contains(randNumberRepr+"")) {
+                        continue;
+                    } else {
+                        bingoCardRepr += randNumberRepr + sep;
+                    }
                 }
-                max = (j + 1) * 15;
-                min = j * 15 + 1;
-                randNumberRepr = getRandomNumberRepr(min, max); // (rand.nextInt(107 - 33) + 33);
-                if (bingoCardRepr.indexOf(randNumberRepr) == -1) {
-                    bingoCardRepr += randNumberRepr + sep;
-                    j++;
-                }
+                col++;
             }
             bingoCardRepr += "\n\n";
         }
@@ -111,7 +118,47 @@ public class BINGO {
         return (char) ((rand.nextInt(max - min) + min) + 33);
     }
 
+    static int getNumberFromRepr(char repr) {
+        return repr - 33;
+    }
+
     static void contains(char repr) {
         int i;
+    }
+
+    static void cls() throws IOException, InterruptedException {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+    }
+
+    static void playTutorial() throws IOException, InterruptedException {
+        System.out.println("Hello! Welcome to the BINGO tutorial! (ENTER to continue)");
+        scanner.nextLine();
+        System.out.print("Let's start with the mechanics:\nBINGO is a game of chance. (ENTER)");
+        scanner.nextLine();
+        System.out.println("Once every turn, a number is rolled randomly. (ENTER)");
+        scanner.nextLine();
+        System.out.print("""
+                On a BINGO card that looks something like this:
+                12\t\t22\t\t43\t\t51\t\t72
+                5\t\t23\t\t35\t\t57\t\t61
+                9\t\t28\t\tB\t\t48\t\t69
+                11\t\t29\t\t41\t\t49\t\t62
+                6\t\t19\t\t42\t\t50\t\t65
+                if the rolled number is present, the number is marked. (ENTER)
+                """);
+        scanner.nextLine();
+        System.out.print("""
+                The middle, denoted B, is a sort of free square.
+                Consider it already marked. (ENTER)
+                """);
+        scanner.nextLine();
+        System.out.print("""
+                The game is won if five (5) marks in a row is achieved,
+                either vertically, horizontally, or diagonally. (ENTER)
+                """);
+        scanner.nextLine();
+        System.out.println("Good luck and have fun! (ENTER)");
+        scanner.nextLine();
+        cls();
     }
 }
