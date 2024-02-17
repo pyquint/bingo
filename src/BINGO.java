08c556010c20edb0d3b02f91b78f1ae9a9f2d12c
Rdoez <160138906+Rodoez@users.noreply.github.com>
2024-02-17 19:30:53 +0800

    Revert "Experiment"

--- src/BINGO.java
+++ src/BINGO.java
@@ -27,6 +27,7 @@ public class BINGO {
             Tara BINGO! (ENTER to continue)""");
 
         // HELP MODULE
+        // playTutorial();
 
         // INITIALIZATION OF VARIABLES
         createBingoCardRepr();
